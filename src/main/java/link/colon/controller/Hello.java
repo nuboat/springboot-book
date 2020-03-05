7406731eae3a0b1d78ccd821562d231830ae49ad
Peerapat <nb@beid.io>
2020-03-05 15:17:25 +0700

    Removed Autowired

--- src/main/java/link/colon/controller/Hello.java
+++ src/main/java/link/colon/controller/Hello.java
@@ -1,19 +1,29 @@
 package link.colon.controller;
 
+import link.colon.component.HelloWorld;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 class Hello {
 
+    private final HelloWorld c;
+
     @GetMapping(value = "/hi")
-    String hi() {
+    private String hi() {
         return "Yo !";
     }
 
+    @GetMapping(value = "/helloWorld")
+    private String helloWorld() {
+        return c.sayHi();
+    }
+
     @GetMapping(value = "/echo/{msg}")
-    String echo(@PathVariable("msg") final String msg) {
+    private String echo(@PathVariable("msg") final String msg) {
         return msg;
     }
 
