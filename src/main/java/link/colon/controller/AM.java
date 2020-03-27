edc21a65b333bd5b21c229e5ff9112509c50de95
Peerapat <nb@beid.io>
2020-03-27 12:52:55 +0700

    stress test

--- src/main/java/link/colon/controller/AM.java
+++ src/main/java/link/colon/controller/AM.java
@@ -1,20 +1,26 @@
 package link.colon.controller;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @Slf4j
 @RestController
-@RequiredArgsConstructor
 class AM {
+
+    private final Map<String, String> store;
+
+    AM() {
+        store = Map.of("123", "นบ.", "124", "บน.");
+    }
 
     @GetMapping(value = "/verify/{token}")
     private String verify(@PathVariable("token") final String token) {
         log.info("Authoen by {}", token);
-        return "123".equals(token) ? "pass" : "unauthorized";
+        return store.getOrDefault("token", null);
     }
 
 }
